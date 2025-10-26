e7354f336f98f1dd219f9c33404d7a1e3da8fca9
Oleksandr Borysiuk <alexboris1004@gmail.com>
2025-10-26 04:08:44 +0100

    Commit 2(header)
    Add header

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('click', documentActions)
+
+function documentActions(e) {
+	const targetElemnt = e.target
+	if (targetElemnt.closest('.menu__icon')) {
+		document.documentElement.classList.toggle('menu-open')
+	}
+}
